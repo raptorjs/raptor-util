9af14705dfd8ee05efaa4e4d71da745c49c9fd42
Patrick Steele-Idem <pnidem@gmail.com>
2014-07-25 16:10:35 -0600

    Minor formatting

--- createError.js
+++ createError.js
@@ -1,24 +1,19 @@
 module.exports = function(message, cause) {
-    var error,
-        argsLen = arguments.length,
-        E = Error;
+    var error;
+    var argsLen = arguments.length;
+    var E = Error;
     
     if (argsLen == 2) {
         error = message instanceof E ? message : new E(message);
         if (error.stack) {
             error.stack += '\nCaused by: ' + (cause.stack || cause);
-        }
-        else {
+        } else {
             error._cause = cause;    
         }
-    }
-    else if (argsLen == 1) {
-        if (message instanceof E)
-        {
+    } else if (argsLen == 1) {
+        if (message instanceof E) {
             error = message;
-        }
-        else
-        {
+        } else {
             error = new E(message);
         }
     }
